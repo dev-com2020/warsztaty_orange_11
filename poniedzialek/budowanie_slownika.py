c56f67a5890178a6cc598688335bb5ec4f1cdd58
devcom2020 <kwp@tlen.pl>
2023-01-11 14:42:26 +0100

    poniedzialek 14:42

--- poniedzialek/budowanie_slownika.py
+++ poniedzialek/budowanie_slownika.py
@@ -5,6 +5,24 @@ def build_person(first_name, last_name, age=None):
     return person
 
 
-print(build_person('tomasz', 'kaniecki'))
-print(build_person('tomasz', 'kowalski', 33))
+def get_formatted_name(first, last):
+    full_name = f"{first} {last}"
+    return full_name
+
+
+# while True:
+#     print('Podaj imię i nazwisko:')
+#     print('Wpisz "q" aby zakończyć')
+#     f_name = input("IMIĘ: ")
+#     if f_name == 'q':
+#         break
+#     l_name = input("NAZWISKO: ")
+#     if l_name == 'q':
+#         break
+#     formatted_name = get_formatted_name(f_name, l_name)
+#     print(f"Witaj {formatted_name}")
+
+# print(build_person('tomasz', 'kaniecki'))
+# print(build_person('tomasz', 'kowalski', 33))
+
 
---
+++ poniedzialek/start.py
@@ -0,0 +1,7 @@
+from pakiet import drukarka
+
+designs = ['telefon', 'robot', 'sześcian']
+models = []
+print("coś wykonujemy...")
+drukarka.print_models(designs, models)
+drukarka.show_completed(models)
