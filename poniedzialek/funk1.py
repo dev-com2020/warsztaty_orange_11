699a39f0a31c99d8aa8a901445a3e6f2107f7043
devcom2020 <kwp@tlen.pl>
2023-01-11 11:45:15 +0100

    poniedzialek 11:45

--- poniedzialek/funk1.py
+++ poniedzialek/funk1.py
@@ -2,6 +2,16 @@ def powitanie(imie: str):
     print(f"Witaj, {imie.title()} !")
 
 
+def powitanie2(imie: str, wiek=18, *liczba):
+    '''Funkcja zwraca łańcuch znaków z parametrami'''
+    return f"Witaj, {imie.title()}, masz {wiek} lat. A magiczna liczba to {liczba} !"
+
+
 powitanie("tomek")
 powitanie(str(456))
+print(powitanie2("jarek"))
+imie = powitanie2("jarek", 25, 5, 55, 22, 11, 33)
 
+imiona = []
+imiona.append(imie)
+print(imiona)
