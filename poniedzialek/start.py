7f74a427c8589ae6a988b547a669b782b6148b19
devcom2020 <kwp@tlen.pl>
2023-01-12 13:54:22 +0100

    wtorek 13:54

--- poniedzialek/start.py
+++ poniedzialek/start.py
@@ -2,16 +2,18 @@ from pakiet import drukarka
 
 designs = ['telefon', 'robot', 'sześcian']
 models = []
-# print("coś wykonujemy...")
-# drukarka.print_models(designs, models)
-# drukarka.show_completed(models)
+print("coś wykonujemy...")
+drukarka.print_models(designs, models)
+drukarka.show_completed(models)
 
-global_adj: float
-global_adj = 3.6
+input("Aby zakończyć, wciśnij ENTER")
 
-
-def some_fun(a: float, b: float, t: float) -> float:
-    return a + b * t + global_adj
-
-
-(print(some_fun(4, 3, 2)))
+# global_adj: float
+# global_adj = 3.6
+#
+#
+# def some_fun(a: float, b: float, t: float) -> float:
+#     return a + b * t + global_adj
+#
+#
+# (print(some_fun(4, 3, 2)))
