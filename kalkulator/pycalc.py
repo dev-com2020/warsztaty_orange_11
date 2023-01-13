c0f2d29757a1f82cf8ff485a86af0709c20ee72b
devcom2020 <kwp@tlen.pl>
2023-01-13 14:37:14 +0100

    piatek 14:37

--- kalkulator/pycalc.py
+++ kalkulator/pycalc.py
@@ -11,8 +11,9 @@ from PyQt6.QtWidgets import (
     QWidget,
 )
 
-
 WINDOW_SIZE = 235
+DISPLAY_H = 35
+BUTTON_SIZE = 40
 
 
 class PyCalcWindow(QMainWindow):
@@ -21,8 +22,35 @@ class PyCalcWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("PyCal!")
         self.setFixedSize(WINDOW_SIZE, WINDOW_SIZE)
+        self.generalLayout = QVBoxLayout()
         centralWidget = QWidget(self)
+        centralWidget.setLayout(self.generalLayout)
         self.setCentralWidget(centralWidget)
+        self._createDisplay()
+        self._createButtons()
+
+    def _createDisplay(self):
+        self.display = QLineEdit()
+        self.display.setFixedHeight(DISPLAY_H)
+        self.display.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.display.setReadOnly(True)
+        self.generalLayout.addWidget(self.display)
+
+    def _createButtons(self):
+        self.buttonMap = {}
+        buttonLayout = QGridLayout()
+        keyBoard = [
+            ["7", "8", "9", "/", "C"],
+            ["4", "5", "6", "*", "("],
+            ["1", "2", "3", "-", ")"],
+            ["0", "00", ".", "+", "="],
+        ]
+        for row, keys in enumerate(keyBoard):
+            for col, key in enumerate(keys):
+                self.buttonMap[key] = QPushButton(key)
+                self.buttonMap[key].setFixedSize(BUTTON_SIZE, BUTTON_SIZE)
+                buttonLayout.addWidget(self.buttonMap[key], row, col)
+        self.generalLayout.addLayout(buttonLayout)
 
 
 def main():
